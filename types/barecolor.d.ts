08b2881c2c9aa3b41b0f52ff6e76cabab76abb50
Fabian Volkers <33686912+FabianVolkers@users.noreply.github.com>
2023-10-17 17:50:45 +0200

    Run prettier

--- types/barecolor.d.ts
+++ types/barecolor.d.ts
@@ -1,25 +1,25 @@
 declare module 'barecolor' {
-    const rgb: Barecolor;
-    export default rgb;
+    const rgb: Barecolor
+    export default rgb
 }
 
 export interface Barecolor {
-    black: (s: string) => void;
-    blackln: (s: string) => void;
-    red: (s: string) => void;
-    redln: (s: string) => void;
-    green: (s: string) => void;
-    greenln: (s: string) => void;
-    yellow: (s: string) => void;
-    yellowln: (s: string) => void;
-    blue: (s: string) => void;
-    blueln: (s: string) => void;
-    magenta: (s: string) => void;
-    magentaln: (s: string) => void;
-    cyan: (s: string) => void;
-    cyanln: (s: string) => void;
-    white: (s: string) => void;
-    whiteln: (s: string) => void;
-    gray: (s: string) => void;
-    grayln: (s: string) => void;
+    black: (s: string) => void
+    blackln: (s: string) => void
+    red: (s: string) => void
+    redln: (s: string) => void
+    green: (s: string) => void
+    greenln: (s: string) => void
+    yellow: (s: string) => void
+    yellowln: (s: string) => void
+    blue: (s: string) => void
+    blueln: (s: string) => void
+    magenta: (s: string) => void
+    magentaln: (s: string) => void
+    cyan: (s: string) => void
+    cyanln: (s: string) => void
+    white: (s: string) => void
+    whiteln: (s: string) => void
+    gray: (s: string) => void
+    grayln: (s: string) => void
 }
